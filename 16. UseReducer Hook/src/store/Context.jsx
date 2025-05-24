d729559302abdf8a1cc6fb14ac4de99ac1f3b09d
Muhammad Nouman Arshad <mmnoumanarshad456@gmail.com>
2025-05-24 07:57:38 +0500

    Seperate the UseReducer hook from App.jsx to store folder under the file of Context.jsx. It means seperate the business logic from UI.

--- 16. UseReducer Hook/src/store/Context.jsx
+++ 16. UseReducer Hook/src/store/Context.jsx
@@ -1,5 +1,47 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { createContext } from 'react'
 
 export const Context = createContext()
+const reducer = (state, { type, payload }) => {
+  let newTodo = state;
 
+  if (type === "NEW_ITEM") {
+    newTodo = [...state, { name: payload.todoName, dueDate: payload.todoDate }];
+  } else if (type === "DELETE_ITEM") {
+    newTodo = state.filter((item) => item.name !== payload.todoName);
+  }
+  return newTodo;
+};
+
+const TodoContext = ({children}) => {
+  const [todoItem, dispatch] = useReducer(reducer, []);
+
+  const handleNewTodo = (todoName, todoDate) => {
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: {
+        todoName,
+        todoDate,
+      },
+    };
+    dispatch(newItemAction);
+  };
+
+  const handleDeleteTodo = (todoName) => {
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        todoName,
+      },
+    };
+    dispatch(deleteItemAction);
+  };
+
+  return(
+    <Context.Provider value={{ todoItem, handleNewTodo, handleDeleteTodo }}>
+        {children}
+    </Context.Provider>
+  )
+}
+
+export default TodoContext
